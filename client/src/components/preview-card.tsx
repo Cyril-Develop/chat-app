021a5b32d1352486db2e2f7f7e52bc5616c24d84
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-06-10 19:33:20 +0200

    update profile card

--- client/src/components/preview-card.tsx
+++ client/src/components/preview-card.tsx
@@ -4,14 +4,15 @@ import Image from "@/assets/picture-default.svg";
 const PreviewCard = () => {
   const { username, bio } = useUserStore((state) => state.user);
   return (
-    <article className="flex gap-4 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none">
+    <article className="flex gap-4 h-32 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none">
       <img src={Image} alt="Profile" className="w-24 h-24" />
-      <div>
+      <div className="overflow-hidden">
         <h2 className="font-semibold">{username}</h2>
-        <p>{bio}</p>
+        <p className="line-clamp-3">{bio}</p>
       </div>
     </article>
   );
 };
 
 export default PreviewCard;
+

646bf02b3a9f0e9d8d73972bc67cd706ab944c44
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2025-04-14 01:29:51 +0200

    deploy

--- client/src/store/socket.store.tsx
+++ client/src/store/socket.store.tsx
@@ -20,7 +20,9 @@ export const useSocketStore = create<SocketStore>((set) => {
       if (socket) return;
 
       socket = io(import.meta.env.VITE_REACT_APP_SOCKET_URL, {
+        path: "/socket.io/",
         withCredentials: true,
+        transports: ["websocket", "polling"],
       });
 
       socket.on("connect", () => {

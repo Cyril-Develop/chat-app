cd0899123df14ba776dc149d03baee27cd85139e
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2025-04-14 13:46:15 +0200

    update spy indicator

--- client/src/components/indicator/spy-indicator.tsx
+++ client/src/components/indicator/spy-indicator.tsx
@@ -10,11 +10,11 @@ const SpyIndicator = () => {
   return (
     <div
       className={cn(
-        "fixed top-1 left-1/2 -translate-x-1/2 z-50 flex items-center gap-2 px-3 py-2"
+        "absolute top-2 md:top-6 left-1/2 -translate-x-1/2 z-50 flex items-center gap-2 px-3 py-2"
       )}
       title="Mode espion activé"
     >
-      <Icons.mask width={28} height={28} fill="#fff" />
+      <Icons.mask width={24} height={24} fill="#fff" />
     </div>
   );
 };

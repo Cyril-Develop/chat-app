ebc2b2e3049ef6b21918274c7fc0b58ad858498a
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2025-04-16 13:12:36 +0200

    responsive chat

--- client/src/components/chat/chat-unselected.tsx
+++ client/src/components/chat/chat-unselected.tsx
@@ -11,7 +11,7 @@ const ChatUnselected = () => {
       {isLoading ? (
         <Icons.loader />
       ) : (
-        <div className="flex flex-col h-full justify-center items-center grow  p-2 pl-2 xl:p-2 xl:pl-0 xl:flex-row">
+        <div className="flex flex-col h-full justify-center items-center grow p-4 md:p-10 xl:flex-row">
           <img src={Logo} alt="logo-messaging" className="w-full md:w-1/3" />
 
           <div className="flex flex-col items-center gap-2">

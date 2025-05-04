16d74d9ce553835622d35d416082d3668ec36d32
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2025-05-04 10:22:49 +0200

    fix peer server url

--- peer/server.js
+++ peer/server.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const { PeerServer } = require("peer");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -7,23 +6,16 @@ const PORT = process.env.PEER_PORT || 9000;
 
 const peerServer = PeerServer({
   port: PORT,
-  ssl: {
-    key: fs.readFileSync("/etc/letsencrypt/live/cyril-develop.fr/privkey.pem"),
-    cert: fs.readFileSync(
-      "/etc/letsencrypt/live/cyril-develop.fr/fullchain.pem"
-    ),
-  },
   path: process.env.PEER_PATH,
+  allow_discovery: true,
 });
 
-peerServer.on("connection", (client) => {
+peerServer.on('connection', (client) => {
   console.log(`Un nouveau client est connecté: ${client.id}`);
 });
 
-peerServer.on("disconnect", (client) => {
+peerServer.on('disconnect', (client) => {
   console.log(`Le client avec ID ${client.id} s'est déconnecté`);
 });
 
-console.log(
-  `PeerServer is running at https://cyril-develop.fr:${PORT}/${process.env.PEER_PATH}`
-);
+console.log(`PeerServer is running at http://localhost:${PORT}${process.env.PEER_PATH}`);
